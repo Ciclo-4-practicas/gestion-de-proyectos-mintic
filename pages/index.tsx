a1ef2a2af63b0ecbcbe3e053416483ac801bd2b8
Petitevale <vrestrepol@unal.edu.co>
2021-11-03 13:33:48 -0500

    agregar despliegue de dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Home: NextPage = () => {
   return (
     <div className= 'bg-green-500'>
-      <p> Página de Index prueba dev  </p>
+      <p> Página de Index Prueba Dev</p>
      <Link href="/admin/usuarios">
        <a>Ir a admin usuarios</a>
      </Link>
